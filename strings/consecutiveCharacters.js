cc71d46800141807e82e3ced58daf3dfd4ed39f9
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-11-03 16:58:52 -0800

    consecutiveCharacters

---
+++ strings/consecutiveCharacters.js
@@ -0,0 +1,45 @@
+/*
+Consecutive Characters: https://leetcode.com/problems/consecutive-characters/
+
+Given a string s, the power of the string is the maximum length of a non-empty substring that 
+contains only one unique character.
+
+Return the power of the string.
+
+# Example 1:
+Input: s = "leetcode"
+Output: 2
+Explanation: The substring "ee" is of length 2 with the character 'e' only.
+
+# Example 2:
+Input: s = "abbcccddddeeeeedcba"
+Output: 5
+Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+
+# Example 3:
+Input: s = "triplepillooooow"
+Output: 5
+
+# Example 4:
+Input: s = "hooraaaaaaaaaaay"
+Output: 11
+
+# Example 5:
+Input: s = "tourist"
+Output: 1
+
+*/
+
+const maxPower = (s) => {
+    let max = 1;
+    let currCount = 1;
+    for (let i = 1; i < s.length; i += 1) {
+      if (s[i] === s[i - 1]) {
+        currCount += 1;
+      } else {
+        currCount = 1;
+      }
+      max = Math.max(currCount, max);
+    }
+    return max;
+};
